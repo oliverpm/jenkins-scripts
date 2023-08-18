1edf10dee178a4fa682c52cdb0ad02da8b829d6f
opmeza <opmeza@stefanini.com>
2023-08-17 22:29:50 -0500

    update _opmJobBuildTime

--- _opmJobBuildTime.groovy
+++ _opmJobBuildTime.groovy
@@ -1,6 +1,6 @@
 def n=0
 def num_notnull=0
-def miliseconds_alljobs=0
+def milliseconds_alljobs=0
 
 Jenkins.instance.getAllItems(Job).each(){
   job -> job.isBuildable()   	
@@ -8,23 +8,23 @@ Jenkins.instance.getAllItems(Job).each(){
   n = n + 1
     
   def last_job_duration = job.getLastBuild()?.getDurationString()
-  def last_job_duration_miliseconds = job.getLastBuild()?.getDuration()
-  println n + ".)" + "JOB " + job.getUrl() + " --------> " + last_job_duration + "; MilSeconds:" + last_job_duration_miliseconds
+  def last_job_duration_milliseconds = job.getLastBuild()?.getDuration()
+  println n + ".)" + "JOB " + job.getUrl() + " --------> " + last_job_duration + "; MilliSeconds:" + last_job_duration_milliseconds
   
   if(last_job_duration!=null){
     num_notnull = num_notnull + 1
-    miliseconds_alljobs = miliseconds_alljobs + last_job_duration_miliseconds
+    milliseconds_alljobs = milliseconds_alljobs + last_job_duration_milliseconds
   }
   	     
 }
 println "num_notnull = " + num_notnull + "; null = " + (n-num_notnull)
-println "miliseconds_alljobs = " + miliseconds_alljobs
-println "seconds_alljobs = " + (miliseconds_alljobs/1000)
-println "seconds_average_alljobs = " + (miliseconds_alljobs/1000)/num_notnull
-println "minutes_average_alljobs = " + ((miliseconds_alljobs/1000)/num_notnull)/60
-println "hours_average_alljobs = " + (((miliseconds_alljobs/1000)/num_notnull)/60)/60
+println "milliseconds_alljobs = " + milliseconds_alljobs
+println "seconds_alljobs = " + (milliseconds_alljobs/1000)
+println "seconds_average_alljobs = " + (milliseconds_alljobs/1000)/num_notnull
+println "minutes_average_alljobs = " + ((milliseconds_alljobs/1000)/num_notnull)/60
+println "hours_average_alljobs = " + (((milliseconds_alljobs/1000)/num_notnull)/60)/60
 println "============================================================================================="
-println "hours_alljobs = " + ((miliseconds_alljobs/1000)/60)/60
+println "hours_alljobs = " + ((milliseconds_alljobs/1000)/60)/60
 
 
 return null
